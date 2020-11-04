c045f257198bb28e9cff1e8565b04a9c4840c71b
IlyaLitvin <darnon88@gmail.com>
2020-11-04 14:15:51 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,50 +1,49 @@
-import api from "./apiService.js";
+import apiService from "./apiService.js";
 import template from "./template/template.hbs";
 import { error } from "@pnotify/core";
 import "@pnotify/core/dist/PNotify.css";
 import "@pnotify/core/dist/BrightTheme.css";
 import "./css/style.css";
 
-let btn = document.querySelector(".button");
+let buttonLoadMore = document.querySelector(".button");
 let input = document.querySelector('[type="text"]');
-let ul = document.querySelector(".gallery");
+let list = document.querySelector(".gallery");
 let page = 1;
 
-const addCard = function () {
-  api(input.value, page).then((data) => {
+const newPicture = function () {
+  apiService(input.value, page).then((data) => {
     if (data.hits.length > 0) {
       data.hits.forEach((el) => {
-        ul.insertAdjacentHTML("beforeend", template(el));
-        btn.classList.remove("is-hidden");
+        list.insertAdjacentHTML("beforeend", template(el));
+        buttonLoadMore.classList.remove("is-hidden");
       });
     } else {
-      ul.innerHTML = "";
-      btn.classList.add("is-hidden");
-      error("Such images are not found");
+      list.innerHTML = "";
+      buttonLoadMore.classList.add("is-hidden");
+      error("Sorry, I can't find it");
     }
   });
-
-  ul.innerHTML = "";
+  list.innerHTML = "";
   input.value = null;
 };
 let a = 100;
 const loadMore = function () {
-  api(input.value, (page += 1)).then((data) => {
+  apiService(input.value, (page += 1)).then((data) => {
     data.hits.forEach((el) => {
       document.documentElement > a + 100;
-      ul.insertAdjacentHTML("beforeend", template(el));
+      list.insertAdjacentHTML("beforeend", template(el));
     });
   });
 };
-btn.addEventListener("click", loadMore);
-input.addEventListener("change", addCard);
+buttonLoadMore.addEventListener("click", loadMore);
+input.addEventListener("change", newPicture);
 window.addEventListener("scroll", () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
   if (clientHeight + scrollTop >= scrollHeight - 1) {
     loadMore();
-    const fun = function () {
+    const scrollFunc = function () {
       window.scrollBy(0, 850);
     };
-    setTimeout(fun, 100);
+    setTimeout(scrollFunc, 100);
   }
 });
